76ebaef8854e0e88d57fa4730374d05f9b94e9ff
Mayur Humbre <mayurhumbre123@gmail.com>
2025-02-24 12:55:50 +0530

    A full update  of the files if left any

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
 const UserSchema = new Schema({
-    username: { type: String, required: true, unique: true },
+    name: { type: String, required: true, unique: true },
+    email: {type: String, required: true, unique: true},
     password: { type: String, required: true },
 });
 export default model('User', UserSchema);
